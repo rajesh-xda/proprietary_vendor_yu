5bd1cfdb4559c630ecd82c82f2cb269b4e1f63cf
Rajesh <rajeshramece@gmail.com>
2019-04-01 13:00:20 +0530

    tomato:Remove unused camera firmware

--- tomato/tomato-vendor.mk
+++ tomato/tomato-vendor.mk
@@ -30,9 +30,6 @@ PRODUCT_COPY_FILES += \
     vendor/yu/tomato/proprietary/vendor/bin/time_daemon:system/vendor/bin/time_daemon \
     vendor/yu/tomato/proprietary/vendor/firmware/a420_pfp.fw:system/vendor/firmware/a420_pfp.fw \
     vendor/yu/tomato/proprietary/vendor/firmware/a420_pm4.fw:system/vendor/firmware/a420_pm4.fw \
-    vendor/yu/tomato/proprietary/vendor/firmware/cpp_firmware_v1_1_1.fw:system/vendor/firmware/cpp_firmware_v1_1_1.fw \
-    vendor/yu/tomato/proprietary/vendor/firmware/cpp_firmware_v1_1_6.fw:system/vendor/firmware/cpp_firmware_v1_1_6.fw \
-    vendor/yu/tomato/proprietary/vendor/firmware/cpp_firmware_v1_2_0.fw:system/vendor/firmware/cpp_firmware_v1_2_0.fw \
     vendor/yu/tomato/proprietary/vendor/firmware/cpp_firmware_v1_4_0.fw:system/vendor/firmware/cpp_firmware_v1_4_0.fw \
     vendor/yu/tomato/proprietary/vendor/firmware/venus.b00:system/vendor/firmware/venus.b00 \
     vendor/yu/tomato/proprietary/vendor/firmware/venus.b01:system/vendor/firmware/venus.b01 \
